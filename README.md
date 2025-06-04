11843e1d7a1876101d21ecef8c30bceeb2d85022
PfoRsten <marvin-hauke@hotmail.de>
2025-06-04 20:57:32 +0200

    added some fancy typer stuff and splitted project files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PDF_OCR
+
+# Autocompletion For zsh
+
+Run this command for Autocompletion in zsh
+
+```bash
+eval "$(python main.py --install-completion zsh)"
+```
+
---
+++ Src/cli.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import typer
+
+from ocr_processor import process_pdf
+
+app = typer.Typer()
+
+
+@app.command()
+def run(
+    pdf_path: Path = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        help="Path to the PDF file to process.",
+    )
+):
+    process_pdf(
+        pdf_path=pdf_path,
+        temp_dir=Path("temp_pages"),
+        output_dir=Path("output_pages"),
+        output_pdf=Path("Output_dir/annotated_output.pdf"),
+    )
+
+
+if __name__ == "__main__":
+    app()
